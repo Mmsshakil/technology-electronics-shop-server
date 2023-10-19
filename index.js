2ff92408cac7f8a7a6e0bf0b66add24679ecdbc2
Mmsshakil <softhandshakil@gmail.com>
2023-10-19 21:35:54 +0600

    product added to the server done

--- index.js
+++ index.js
@@ -33,7 +33,16 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const productCollection = client.db('productDB').collection('product');
 
+        // add product
+        app.post('/product', async(req, res) =>{
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+
+        })
 
 
 
