d74a1dc488b2c732acdf683aaa6d63dccdd132b4
Mmsshakil <softhandshakil@gmail.com>
2023-10-20 01:28:19 +0600

    update link problem fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -51,7 +51,14 @@ async function run() {
             res.send(result);
         } )
 
-
+        // update a product
+        app.get('/products/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            // console.log(query);
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
 
 
 
